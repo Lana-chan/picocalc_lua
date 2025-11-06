e9800eeccb139583f7487b41cc16f24da5afddb9
maple "mavica" syrup <Lana-chan@users.noreply.github.com>
2025-11-06 23:51:43 +0000

    replace LCD SPI with PIO (#1)
    * break PSRAM into separate file so other drivers can use it
    
    * clean up lcd driver
    
    * Revert "break PSRAM into separate file so other drivers can use it"
    
    This reverts commit a9d992056e169f69811b7d72892c88032cd114e0.
    
    * replace LCD SPI with PIO

--- drivers/lcd.c
+++ drivers/lcd.c
@@ -7,7 +7,8 @@
 #include "pico/time.h"
 
 #include "hardware/gpio.h"
-#include "hardware/spi.h"
+#include "hardware/pio.h"
+#include "st7789_lcd.pio.h"
 #include "psram_spi.h"
 
 #include "lcd.h"
@@ -22,63 +23,95 @@
 #define LCD_CS  13
 #define LCD_DC  14
 #define LCD_RST 15
+#define LCD_PIO pio1
+#define SERIAL_CLK_DIV 1.f
 
 void(*lcd_draw_ptr) (u16*,int,int,int,int);
 void(*lcd_fill_ptr) (u16,int,int,int,int);
 void(*lcd_point_ptr) (u16,int,int);
 void(*lcd_clear_ptr) (void);
+void(*lcd_buffer_read_ptr) (int);
 psram_spi_inst_t psram_spi;
 psram_spi_inst_t* async_spi_inst;
+
+uint lcd_sm = 0;
+uint lcd_offset;
 
 uint8_t* framebuffer;
 int framebuffer_mode;
 
-static int lcd_write_spi(void *buf, size_t len) {
-	return spi_write_blocking(spi1, buf, len);
+#define LCD_TMPBUF_SIZE LCD_WIDTH*2
+uint16_t lcd_tmpbuf[LCD_TMPBUF_SIZE];
+
+static inline void lcd_set_dc_cs(bool dc, bool cs) {
+	gpio_put_masked((1u << LCD_DC) | (1u << LCD_CS), !!dc << LCD_DC | !!cs << LCD_CS);
 }
 
-static inline void lcd_write_spi_dc(void *buf, size_t len, int dc) {
-	gpio_put(LCD_DC, dc);
-	lcd_write_spi(buf, len);
-}
-
-#define NUMARGS(...)  (sizeof((int[]){__VA_ARGS__}) / sizeof(int))
-static int lcd_write_reg_num(int len, ...) {
-	u8 buf[128];
-	va_list args;
-	int i;
-
-	gpio_put(LCD_DC, 0);
-	va_start(args, len);
-	*buf = (u8)va_arg(args, unsigned int);
-	spi_write_blocking(spi1, buf, sizeof(u8));
-	len--;
-
-	gpio_put(LCD_DC, 1);
-	if (len > 0) {
-		for (i = 0; i < len; i++) buf[i] = (u8)va_arg(args, unsigned int);
-		spi_write_blocking(spi1, buf, len);
+static inline void lcd_write_cmd(const uint8_t *cmd, size_t count) {
+	st7789_lcd_wait_idle(LCD_PIO, lcd_sm);
+	lcd_set_dc_cs(0, 0);
+	st7789_lcd_put(LCD_PIO, lcd_sm, *cmd++);
+	if (count >= 2) {
+		st7789_lcd_wait_idle(LCD_PIO, lcd_sm);
+		lcd_set_dc_cs(1, 0);
+		for (size_t i = 0; i < count - 1; ++i)
+			st7789_lcd_put(LCD_PIO, lcd_sm, *cmd++);
 	}
-	va_end(args);
-
-	return 0;
-}
-#define lcd_write_reg(...) \
-		lcd_write_reg_num(NUMARGS(__VA_ARGS__), __VA_ARGS__)
-
-#define REGION_READ 0
-#define REGION_WRITE 1
-
-static void lcd_set_region(int x1, int y1, int x2, int y2, int rw) {
-	gpio_put(LCD_CS, 0);
-	lcd_write_reg(0x2A, (x1 >> 8), (x1 & 0xFF), (x2 >> 8), (x2 & 0xFF));
-	lcd_write_reg(0x2B, (y1 >> 8), (y1 & 0xFF), (y2 >> 8), (y2 & 0xFF));
-	if (rw) lcd_write_reg(0x2C);
-	else lcd_write_reg(0x2E);
+	st7789_lcd_wait_idle(LCD_PIO, lcd_sm);
+	lcd_set_dc_cs(0, 1);
 }
 
-static int lcd_write_data(u8* buf, int len) {
-	return lcd_write_spi(buf, len);
+static inline void lcd_write16(const uint16_t *data, size_t count) {
+	uint16_t color;
+	for (size_t i = 0; i < count; ++i) {
+		color = *data++;
+		st7789_lcd_put(LCD_PIO, lcd_sm, color >> 8);
+		st7789_lcd_put(LCD_PIO, lcd_sm, color & 0xff);
+	}
+}
+
+static inline void lcd_initcmd(const uint8_t *init_seq) {
+	const uint8_t *cmd = init_seq;
+	while (*cmd) {
+		lcd_write_cmd(cmd + 2, *cmd);
+		sleep_ms(*(cmd + 1) * 5);
+		cmd += *cmd + 2;
+	}
+}
+
+static void lcd_set_region(int x1, int y1, int x2, int y2) {
+	lcd_set_dc_cs(0, 0);
+	const uint8_t cmd1[] = {0x2A, (x1 >> 8), (x1 & 0xFF), (x2 >> 8), (x2 & 0xFF)};
+	const uint8_t cmd2[] = {0x2B, (y1 >> 8), (y1 & 0xFF), (y2 >> 8), (y2 & 0xFF)};
+	lcd_write_cmd(cmd1, 5);
+	lcd_write_cmd(cmd2, 5);
+	uint8_t cmd = 0x2c; // RAMWR
+	lcd_write_cmd(&cmd, 1);
+	lcd_set_dc_cs(1, 0);
+}
+
+void lcd_blank() {
+	// Enter Sleep
+	uint8_t cmd = 0x10;
+	lcd_write_cmd(&cmd, 1);
+}
+
+void lcd_unblank() {
+	// Exit Sleep
+	uint8_t cmd = 0x11;
+	lcd_write_cmd(&cmd, 1);
+}
+
+void lcd_on() {
+	// Display on
+	uint8_t cmd = 0x29;
+	lcd_write_cmd(&cmd, 1);
+}
+
+void lcd_off() {
+	// Display off
+	uint8_t cmd = 0x29;
+	lcd_write_cmd(&cmd, 1);
 }
 
 static inline void normalize_coords(int* x, int* y, int* width, int* height, int scrheight) {
@@ -90,36 +123,25 @@ static inline void normalize_coords(int* x, int* y, int* width, int* height, int
 
 static void lcd_direct_draw(u16* pixels, int x, int y, int width, int height) {
 	normalize_coords(&x, &y, &width, &height, MEM_HEIGHT);
-	lcd_set_region(x, y, x + width - 1, y + height - 1, REGION_WRITE);
+	lcd_set_region(x, y, x + width - 1, y + height - 1);
 
-	//lcd_write_data((u8*) pixels, width * height * 2);
-	spi_set_format(spi1, 16, 0, 0, SPI_MSB_FIRST);
-	spi_write16_blocking(spi1, pixels, width * height);
-	spi_set_format(spi1, 8, 0, 0, SPI_MSB_FIRST);
-	gpio_put(LCD_CS, 1);
+	lcd_write16(pixels, width * height);
+
+	st7789_lcd_wait_idle(LCD_PIO, lcd_sm);
+	lcd_set_dc_cs(0, 1);
 }
 
 static void lcd_direct_fill(u16 color, int x, int y, int width, int height) {
 	normalize_coords(&x, &y, &width, &height, MEM_HEIGHT);
-	lcd_set_region(x, y, x + width - 1, y + height - 1, REGION_WRITE);
-
-	gpio_put(LCD_DC, 1);
-	u16 color2 = (color >> 8) | (color << 8);
-	spi_set_format(spi1, 16, 0, 0, SPI_MSB_FIRST);
-
-	u16 buf[LCD_WIDTH];
-	int remaining = width * height;
-	int chunk_size = remaining < LCD_WIDTH ? remaining : LCD_WIDTH;
-	for (int i = 0; i < chunk_size; i++) buf[i] = color;
-
-	while (remaining > chunk_size) {
-		spi_write16_blocking(spi1, buf, chunk_size);
-		remaining -= chunk_size;
+	lcd_set_region(x, y, x + width - 1, y + height - 1);
+	
+	for (size_t i = 0; i < width * height; ++i) {
+		st7789_lcd_put(LCD_PIO, lcd_sm, color >> 8);
+		st7789_lcd_put(LCD_PIO, lcd_sm, color & 0xff);
 	}
-	spi_write16_blocking(spi1, buf, remaining);
-	//for (int i = 0; i < width * height; i++) spi_write16_blocking(spi1, &color2, 1);
-	spi_set_format(spi1, 8, 0, 0, SPI_MSB_FIRST);
-	gpio_put(LCD_CS, 1);
+
+	st7789_lcd_wait_idle(LCD_PIO, lcd_sm);
+	lcd_set_dc_cs(0, 1);
 }
 
 static void lcd_direct_point(u16 color, int x, int y) {
@@ -147,13 +169,12 @@ static void lcd_psram_draw(u16* pixels, int x, int y, int width, int height) {
 static void lcd_psram_fill(u16 color, int x, int y, int width, int height) {
 	normalize_coords(&x, &y, &width, &height, LCD_HEIGHT);
 
-	u16 buf[10];
 	int remain;
-	for (int i = 0; i < 10; i++) buf[i] = color;
+	for (int i = 0; i < 10; i++) lcd_tmpbuf[i] = color;
 	for (uint32_t iy = y * LCD_WIDTH; iy < (y + height) * LCD_WIDTH; iy += LCD_WIDTH) {
 		remain = width;
 		for (uint32_t ix = x; ix < (x + width); ix+=10) {
-			psram_write(&psram_spi, (iy + ix)<<1, (uint8_t*)buf, (remain < 10 ? remain<<1 : 20));
+			psram_write(&psram_spi, (iy + ix)<<1, (uint8_t*)lcd_tmpbuf, (remain < 10 ? remain<<1 : 20));
 			remain -= 10;
 		}
 	}
@@ -195,33 +216,30 @@ static void lcd_ram_clear() {
 	memset(framebuffer, 0, LCD_WIDTH * LCD_HEIGHT);
 }
 
+static inline void lcd_buffer_read_psram(int y) {
+	for (int x = 0; x < LCD_TMPBUF_SIZE; x+=10) {
+		psram_read(&psram_spi, (x+y)<<1, (uint8_t*)(lcd_tmpbuf + x), 20);
+	}
+}
+
+static inline void lcd_buffer_read_ram(int y) {
+	for (int x = 0; x < LCD_TMPBUF_SIZE; x++) {
+		lcd_tmpbuf[x] = lcd_to16[framebuffer[x + y]];
+	}
+}
+
 void lcd_buffer_blit_local() {
 	if (framebuffer_mode == LCD_BUFFERMODE_DIRECT) return;
 	
-	lcd_set_region(0, 0, 319, 319, REGION_WRITE);
+	lcd_set_region(0, 0, 319, 319);
 
-	u16 buf[LCD_WIDTH];
-	spi_set_format(spi1, 16, 0, 0, SPI_MSB_FIRST);
-	gpio_put(LCD_DC, 1);
-
-	if (framebuffer_mode == LCD_BUFFERMODE_PSRAM) {
-		for (int y = 0; y < LCD_HEIGHT * LCD_WIDTH; y += LCD_WIDTH) {
-			for (int x = 0; x < LCD_WIDTH; x+=10) {
-				psram_read(&psram_spi, (x+y)<<1, (uint8_t*)(buf + x), 20);
-			}
-			spi_write16_blocking(spi1, buf, LCD_WIDTH);
-		}
-	} else if (framebuffer_mode == LCD_BUFFERMODE_RAM) {
-		for (int y = 0; y < LCD_HEIGHT * LCD_WIDTH; y += LCD_WIDTH) {
-			for (int x = 0; x < LCD_WIDTH; x++) {
-				buf[x] = lcd_to16[framebuffer[x + y]];
-			}
-			spi_write16_blocking(spi1, buf, LCD_WIDTH);
-		}
+	for (int y = 0; y < LCD_HEIGHT * LCD_WIDTH; y += LCD_TMPBUF_SIZE) {
+		lcd_buffer_read_ptr(y);
+		lcd_write16(lcd_tmpbuf, LCD_TMPBUF_SIZE);
 	}
 
-	spi_set_format(spi1, 8, 0, 0, SPI_MSB_FIRST);
-	gpio_put(LCD_CS, 1);
+	st7789_lcd_wait_idle(LCD_PIO, lcd_sm);
+	lcd_set_dc_cs(0, 1);
 }
 
 void lcd_draw_local(u16* pixels, int x, int y, int width, int height) {
@@ -253,6 +271,7 @@ bool lcd_buffer_enable_local(int mode) {
 		lcd_fill_ptr = &lcd_direct_fill;
 		lcd_point_ptr = &lcd_direct_point;
 		lcd_clear_ptr = &lcd_direct_clear;
+		lcd_buffer_read_ptr = NULL;
 		framebuffer_mode = mode;
 		return true;
 	} else if (mode == LCD_BUFFERMODE_PSRAM) {
@@ -260,6 +279,7 @@ bool lcd_buffer_enable_local(int mode) {
 		lcd_fill_ptr = &lcd_psram_fill;
 		lcd_point_ptr = &lcd_psram_point;
 		lcd_clear_ptr = &lcd_psram_clear;
+		lcd_buffer_read_ptr = &lcd_buffer_read_psram;
 		framebuffer_mode = mode;
 		return true;
 	} else if (mode == LCD_BUFFERMODE_RAM) {
@@ -270,6 +290,7 @@ bool lcd_buffer_enable_local(int mode) {
 				lcd_fill_ptr = &lcd_ram_fill;
 				lcd_point_ptr = &lcd_ram_point;
 				lcd_clear_ptr = &lcd_ram_clear;
+				lcd_buffer_read_ptr = &lcd_buffer_read_ram;
 				framebuffer_mode = mode;
 				return true;
 			}
@@ -281,17 +302,18 @@ bool lcd_buffer_enable_local(int mode) {
 
 void lcd_scroll_local(int lines) {
 	lines %= MEM_HEIGHT;
-	gpio_put(LCD_CS, 0);
-	lcd_write_reg(0x37, (lines >> 8), (lines & 0xFF));
-	gpio_put(LCD_CS, 1);
+	uint8_t cmd[] = {0x37, (lines >> 8), (lines & 0xFF)};
+	lcd_write_cmd(cmd, 3);
 }
 
 void lcd_setup_scrolling(int top_fixed_lines, int bottom_fixed_lines) {
 	int vertical_scrolling_area = LCD_HEIGHT - (top_fixed_lines + bottom_fixed_lines);
-	gpio_put(LCD_CS, 0);
-	lcd_write_reg(0x33, (top_fixed_lines >> 8), (top_fixed_lines & 0xFF), (vertical_scrolling_area >> 8), 
-		(vertical_scrolling_area & 0xFF), (bottom_fixed_lines >> 8), (bottom_fixed_lines & 0xff));
-	gpio_put(LCD_CS, 1);
+	const uint8_t cmd[] = {0x33,
+		(top_fixed_lines >> 8), (top_fixed_lines & 0xFF),
+		(vertical_scrolling_area >> 8), (vertical_scrolling_area & 0xFF),
+		(bottom_fixed_lines >> 8), (bottom_fixed_lines & 0xff)
+	};
+	lcd_write_cmd(cmd, 7);
 }
 
 font_t font = {
@@ -377,91 +399,47 @@ void lcd_printf(int x, int y, u16 fg, u16 bg, const char* format, ...) {
 	}
 }
 
-
-#define LCD_SPI_SPEED   (75 * 1000 * 1000)
-#define PORTCLR             1
-#define PORTSET             2
-#define PORTINV             3
-#define LAT                 4
-#define LATCLR              5
-#define LATSET              6
-#define LATINV              7
-#define ODC                 8
-#define ODCCLR              9
-#define ODCSET              10
-#define CNPU                12
-#define CNPUCLR             13
-#define CNPUSET             14
-#define CNPUINV             15
-#define CNPD                16
-#define CNPDCLR             17
-#define CNPDSET             18
-#define ANSELCLR            -7
-#define ANSELSET            -6
-#define ANSELINV            -5
-#define TRIS                -4
-#define TRISCLR             -3
-#define TRISSET             -2
-
-static void pin_set_bit(int pin, unsigned int offset) {
-	switch (offset) {
-		case LATCLR:
-			gpio_set_pulls(pin, false, false);
-			gpio_pull_down(pin);
-			gpio_put(pin, 0);
-			return;
-		case LATSET:
-			gpio_set_pulls(pin, false, false);
-			gpio_pull_up(pin);
-			gpio_put(pin, 1);
-			return;
-		case LATINV:
-			gpio_xor_mask(1 << pin);
-			return;
-		case TRISSET:
-			gpio_set_dir(pin, GPIO_IN);
-			sleep_us(2);
-			return;
-		case TRISCLR:
-			gpio_set_dir(pin, GPIO_OUT);
-			gpio_set_drive_strength(pin, GPIO_DRIVE_STRENGTH_12MA);
-			sleep_us(2);
-			return;
-		case CNPUSET:
-			gpio_set_pulls(pin, true, false);
-			return;
-		case CNPDSET:
-			gpio_set_pulls(pin, false, true);
-			return;
-		case CNPUCLR:
-		case CNPDCLR:
-			gpio_set_pulls(pin, false, false);
-			return;
-		case ODCCLR:
-			gpio_set_dir(pin, GPIO_OUT);
-			gpio_put(pin, 0);
-			sleep_us(2);
-			return;
-		case ODCSET:
-			gpio_set_pulls(pin, true, false);
-			gpio_set_dir(pin, GPIO_IN);
-			sleep_us(2);
-			return;
-		case ANSELCLR:
-			gpio_set_function(pin, GPIO_FUNC_SIO);
-			gpio_set_dir(pin, GPIO_IN);
-			return;
-		default:
-			printf("unknown pin_set_bit command\n");
-			break;
-	}
-}
+static const uint8_t st7789_init_seq[] = {
+	// Positive Gamma Control
+	16, 0, 0xE0, 0x00, 0x03, 0x09, 0x08, 0x16, 0x0A, 0x3F, 0x78, 0x4C, 0x09, 0x0A, 0x08, 0x16, 0x1A, 0x0F,
+	// Negative Gamma Control
+	16, 0, 0xE1, 0x00, 0x16, 0x19, 0x03, 0x0F, 0x05, 0x32, 0x45, 0x46, 0x04, 0x0E, 0x0D, 0x35, 0x37, 0x0F,
+	// Power Control 1
+	3, 0, 0xC0, 0x17, 0x15,
+	// Power Control 2
+	2, 0, 0xC1, 0x41,
+	// VCOM Control
+	4, 0, 0xC5, 0x00, 0x12, 0x80,
+	// Memory Access Control (0x48=BGR, 0x40=RGB)
+	2, 0, 0x36, 0x48,
+	// Pixel Interface Format  16 bit colour for SPI
+	2, 0, 0x3A, 0x55,
+	// Interface Mode Control
+	2, 0, 0xB0, 0x00,
+	// 60Hz
+	3, 0, 0xB1, 0xD0, 0x11,
+	// Invert colors on
+	1, 0, 0x21,
+	// Display Inversion Control
+	2, 0, 0xB4, 0x02,
+	// Display Function Control
+	4, 0, 0xB6, 0x02, 0x02, 0x3B,
+	// Entry Mode Set
+	2, 0, 0xB7, 0xC6,
+	2, 0, 0xE9, 0x00,
+	// Adjust Control 3
+	5, 0, 0xF7, 0xA9, 0x51, 0x2C, 0x82,
+	// Exit Sleep
+	1, 0, 0x11,
+	// Terminate list
+	0
+};
 
 void lcd_init() {
 	// Init GPIO
 	gpio_init(LCD_SCK);
 	gpio_init(LCD_TX);
-	gpio_init(LCD_RX);
+	//gpio_init(LCD_RX);
 	gpio_init(LCD_CS);
 	gpio_init(LCD_DC);
 	gpio_init(LCD_RST);
@@ -473,55 +451,22 @@ void lcd_init() {
 	gpio_set_dir(LCD_DC, GPIO_OUT);
 	gpio_set_dir(LCD_RST, GPIO_OUT);
 
-	// Init SPI
-	spi_init(spi1, LCD_SPI_SPEED);
-	gpio_set_function(LCD_SCK, GPIO_FUNC_SPI);
-	gpio_set_function(LCD_TX, GPIO_FUNC_SPI);
-	gpio_set_function(LCD_RX, GPIO_FUNC_SPI);
-	gpio_set_input_hysteresis_enabled(LCD_RX, true);
+	// Init PIO
+	lcd_offset = pio_add_program(LCD_PIO, &st7789_lcd_program);
+	st7789_lcd_program_init(LCD_PIO, lcd_sm, lcd_offset, LCD_TX, LCD_SCK, SERIAL_CLK_DIV);
 
-	gpio_put(LCD_CS, 1);
+	lcd_set_dc_cs(0, 1);
 	gpio_put(LCD_RST, 1);
 
 	// Reset controller
-	pin_set_bit(LCD_RST, LATSET);
-	sleep_ms(10);
-	pin_set_bit(LCD_RST, LATCLR);
-	sleep_ms(10);
-	pin_set_bit(LCD_RST, LATSET);
-	sleep_ms(200);
+	gpio_put(LCD_RST, 0);
+	busy_wait_us(20); // 20µs reset pulse (10µs minimum)
+	gpio_put(LCD_RST, 1);
+	busy_wait_us(120000); // 5ms required after reset, but 120ms needed before sleep out command
 
 	// Setup LCD
-	gpio_put(LCD_CS, 0);
-	// Positive Gamma Control
-	lcd_write_reg(0xE0, 0x00, 0x03, 0x09, 0x08, 0x16, 0x0A, 0x3F, 0x78, 0x4C, 0x09, 0x0A, 0x08, 0x16, 0x1A, 0x0F);
-
-	// Negative Gamma Control
-	lcd_write_reg(0xE1, 0x00, 0x16, 0x19, 0x03, 0x0F, 0x05, 0x32, 0x45, 0x46, 0x04, 0x0E, 0x0D, 0x35, 0x37, 0x0F);
-
-	lcd_write_reg(0xC0, 0x17, 0x15);          // Power Control 1
-	lcd_write_reg(0xC1, 0x41);                // Power Control 2
-	lcd_write_reg(0xC5, 0x00, 0x12, 0x80);    // VCOM Control
-	lcd_write_reg(0x36, 0x48);                // Memory Access Control (0x48=BGR, 0x40=RGB)
-	lcd_write_reg(0x3A, 0x55);                // Pixel Interface Format  16 bit colour for SPI
-	lcd_write_reg(0xB0, 0x00);                // Interface Mode Control
-
-	// Frame Rate Control
-	//lcd_write_reg(0xB1, 0xA0);
-	lcd_write_reg(0xB1, 0xD0, 0x11);          // 60Hz
-	//lcd_write_reg(0xB1, 0xD0, 0x14);          // 90Hz
-	lcd_write_reg(0x21);                      // Invert colors on
-
-	lcd_write_reg(0xB4, 0x02);                // Display Inversion Control
-	lcd_write_reg(0xB6, 0x02, 0x02, 0x3B);    // Display Function Control
-	lcd_write_reg(0xB7, 0xC6);                // Entry Mode Set
-	lcd_write_reg(0xE9, 0x00);
-	lcd_write_reg(0xF7, 0xA9, 0x51, 0x2C, 0x82);  // Adjust Control 3
-	lcd_write_reg(0x11);                      // Exit Sleep
-	//sleep_ms(120);
-	//lcd_write_reg(0x29);                      // Display on
-	//sleep_ms(120);
-	gpio_put(LCD_CS, 1);
+	lcd_initcmd(st7789_init_seq);
+	lcd_set_dc_cs(0, 1);
 
 	psram_spi = psram_spi_init(pio0, -1);
 	lcd_buffer_enable(0);
@@ -530,30 +475,6 @@ void lcd_init() {
 
 	lcd_clear();
 	lcd_on();
-}
-
-void lcd_blank() {
-	gpio_put(LCD_CS, 0);
-	lcd_write_reg(0x10);                      // Enter Sleep
-	gpio_put(LCD_CS, 1);
-}
-
-void lcd_unblank() {
-	gpio_put(LCD_CS, 0);
-	lcd_write_reg(0x11);                      // Exit Sleep
-	gpio_put(LCD_CS, 1);
-}
-
-void lcd_on() {
-	gpio_put(LCD_CS, 0);
-	lcd_write_reg(0x29);                      // Display on
-	gpio_put(LCD_CS, 1);
-}
-
-void lcd_off() {
-	gpio_put(LCD_CS, 0);
-	lcd_write_reg(0x29);                      // Display off
-	gpio_put(LCD_CS, 1);
 }
 
 int lcd_fifo_receiver(uint32_t message) {
